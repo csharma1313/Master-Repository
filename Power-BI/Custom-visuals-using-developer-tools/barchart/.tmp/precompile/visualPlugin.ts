05d9d2bb0a79cd8d625ae5e4daa1f0f83fdeff55
csharma1313 <sharma.charu.1313@gmail.com>
2019-05-06 20:01:29 +0200

    fjffggj

---
+++ Power-BI/Custom-visuals-using-developer-tools/barchart/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var barchart0DF8AB49A5884B8B9F965E79780EB4AE = {
+        name: 'barchart0DF8AB49A5884B8B9F965E79780EB4AE',
+        displayName: 'barchart',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.6.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.barchart0DF8AB49A5884B8B9F965E79780EB4AE.Visual(options),
+        custom: true
+    };
+}
