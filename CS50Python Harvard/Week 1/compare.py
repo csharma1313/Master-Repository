c9512695c0efdc11c48b6ae041b549d4fb4467e3
Sharma <charu.sharma@sap.com>
2026-07-14 15:04:35 +0200

    adding grade, house, parity

--- CS50Python Harvard/Week 1/compare.py
+++ CS50Python Harvard/Week 1/compare.py
@@ -4,8 +4,14 @@ y = int(input("Whats y? "))
 if x < y:
     print("x is less than y")
 
-if x > y:
+elif x > y:
     print("x is greater than y")
 
-if x == y:
+else:
     print("x is equal to y")
+
+if x < y or x > y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
