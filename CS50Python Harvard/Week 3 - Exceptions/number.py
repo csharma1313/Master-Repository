a4c211829fb9a03050435077954bb9c5fca201c9
Sharma <charu.sharma@sap.com>
2026-08-03 21:08:07 +0200

    Exceptions lecture

--- CS50Python Harvard/Week 3 - Exceptions/number.py
+++ CS50Python Harvard/Week 3 - Exceptions/number.py
@@ -1,14 +1,14 @@
 
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            return int(input("What's x? "))
+            return int(input(prompt))
         except ValueError:
             #print("x is not an integer")
             pass
         
 def main():
-    x = get_int()
+    x = get_int("What's x? ")
     print(f"x is {x}")
 
 main()
